f89a5788e1f23a33d588a2059d394cdb7a5d9423
yon <valandil.yon@gmail.com>
2017-05-05 01:32:16 +0200

    upd

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -14,16 +14,26 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	int rd;
-  char buf[BUF_SIZE + 1];
+	int							rd;
+  char						buf[BUF_SIZE + 1];
+	static t_list		*lst;
+	//t_list					*curr;
 
+	lst = ft_lstnew("", 0);
+	//curr = lst;
   if (fd < 0 || !line)
 		return (-1);
   while (rd > 0)
   {
     rd = read(fd, buf, BUF_SIZE);
     buf[rd] = '\0';
-    ft_putstr(buf);
+		ft_strcpy(lst->content, buf);
+		//if (lst->content == '\n')
+		//{
+			ft_putstr(lst->content);
+			//rd = 0;
+		//}
   }
+	//ft_putstr(lst->content);
   return (1);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -5,23 +5,16 @@ int		main(int argc, char *argv[])
 {
   int r;
   int fd;
-  //char *line;
 
-  //if (!(line = (char*)ft_memalloc(sizeof(char*) * (50 + 1))))
-	//	return (0);
-  if (argc == 2)
+  if (argc > 1)
   {
     fd = open(argv[1], O_RDONLY);
-    //r	=	get_next_line(fd, &line);
     r	=	get_next_line(fd, argv);
     printf("\n\nreturn GNL : %d\n", r);
     if (close(fd))
       write(2, "file close failed\n", 18);
-    //ft_memdel(&line);
   }
-	else if (argc > 2)
-		write(2, "Too many arguments.\n", 20);
 	else
-		write(2, "File name missing.\n", 19);
+		write(2, "arguments missing.\n", 19);
   return (0);
 }
