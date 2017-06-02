0e86d74acab3631c12fbb2ddf5fbf852e8fa2461
Jimmy JOURNE <jjourne@student.42.fr>
2017-06-02 04:31:38 +0200

    upd norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,26 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/29 03:10:37 by jjourne           #+#    #+#             */
-/*   Updated: 2017/05/22 22:49:28 by jjourne          ###   ########.fr       */
+/*   Updated: 2017/06/02 04:29:17 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_fd_list		*fd_search(int fd, t_fd_list **l)
+t_fd_list	*fd_search(int fd, t_fd_list **l)
 {
 	t_fd_list *curr;
 
 	curr = *l;
 	 while (curr)
 	{
-		if(curr->fd == fd)
+		if (curr->fd == fd)
 			return (curr);
 		curr = curr->next;
 	}
 	if (!curr)
 	{
-		curr = (t_fd_list*)ft_memalloc(sizeof(t_fd_list)); //non protégé ?
+		curr = (t_fd_list*)ft_memalloc(sizeof(t_fd_list));
 		curr->fd = fd;
 		curr->i = 0;
 		curr->content = NULL;
@@ -35,11 +35,11 @@ t_fd_list		*fd_search(int fd, t_fd_list **l)
 	return (curr);
 }
 
-void	*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2)
+void		*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2)
 {
-	size_t		i;
-	size_t		j;
-	void			*ns;
+	size_t	i;
+	size_t	j;
+	void	*ns;
 
 	if (!s1 && !s2)
 		return (0);
@@ -63,9 +63,9 @@ void	*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2)
 	return ((unsigned char*)ns);
 }
 
-int	make_line(size_t r, char *b, t_fd_list	*curr, char **line)
+int			make_line(size_t r, char *b, t_fd_list	*curr, char **line)
 {
-	char					*tmp;
+	char *tmp;
 
 	tmp = NULL;
 	if (ft_memchr(b, '\n', r))
@@ -87,13 +87,13 @@ int	make_line(size_t r, char *b, t_fd_list	*curr, char **line)
 	return (0);
 }
 
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	size_t				r;
-	char					b[BUFF_SIZE + 1];
-	static 				t_fd_list	*l = NULL;
+	char				b[BUFF_SIZE + 1];
+	static t_fd_list	*l = NULL;
 	t_fd_list			*curr;
-	char					*tmp;
+	char				*tmp;
 
 	if (fd < 0 || !line)
 		return (-1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,20 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/29 03:10:44 by jjourne           #+#    #+#             */
-/*   Updated: 2017/05/09 05:12:49 by jjourne          ###   ########.fr       */
+/*   Updated: 2017/06/02 04:23:44 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "libft.h"
+# include "libft.h"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/fcntl.h>
 
-#define BUFF_SIZE 4
+# define BUFF_SIZE 4
 
 typedef struct		s_fd_list
 {
@@ -29,9 +29,9 @@ typedef struct		s_fd_list
 	struct s_fd_list	*next;
 }						t_fd_list;
 
-t_fd_list   *fd_search(int fd, t_fd_list **l);
-int	make_line(size_t r, char *b, t_fd_list	*curr, char **line);
-void	*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2);
-int		get_next_line(const int fd, char **line);
+t_fd_list	*fd_search(int fd, t_fd_list **l);
+int			make_line(size_t r, char *b, t_fd_list	*curr, char **line);
+void		*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2);
+int			get_next_line(const int fd, char **line);
 
 #endif
