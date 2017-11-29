b6c6a9378f70a06208eed79533256f493e17fa86
Yon <valandil.yon@gmail.com>
2017-11-29 22:10:54 +0100

    add ft_memjoin to the lib, add function in static

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-t_fd_list	*fd_search(int fd, t_fd_list **l)
+static t_fd_list	*fd_search(int fd, t_fd_list **l)
 {
 	t_fd_list	*curr;
 
@@ -34,35 +34,7 @@ t_fd_list	*fd_search(int fd, t_fd_list **l)
 	return (curr);
 }
 
-void		*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2)
-{
-	size_t	i;
-	size_t	j;
-	void	*ns;
-
-	if (!s1 && !s2)
-		return (0);
-	if (!s1 || !s2)
-	{
-		ns = ft_memalloc((!s1) ? n2 : n1);
-		if (ns == NULL)
-			return (NULL);
-		ns = ft_memcpy(ns, ((!s1) ? (void*)s2 : (void*)s1), ((!s1) ? n2 : n1));
-		return ((unsigned char*)ns);
-	}
-	i = -1;
-	j = -1;
-	ns = (unsigned char*)ft_memalloc(n1 + n2);
-	if (ns == NULL)
-		return (NULL);
-	while (++i < n1)
-		((unsigned char*)ns)[i] = ((const unsigned char*)s1)[i];
-	while (++j < n2)
-		((unsigned char*)ns)[i++] = ((const unsigned char*)s2)[j];
-	return ((unsigned char*)ns);
-}
-
-int			make_line(t_fd_list *curr, char **line)
+static int			make_line(t_fd_list *curr, char **line)
 {
 	char *tmp;
 
@@ -85,7 +57,7 @@ int			make_line(t_fd_list *curr, char **line)
 	return (0);
 }
 
-int			last_check(t_fd_list *curr, char **line)
+static int			last_check(t_fd_list *curr, char **line)
 {
 	if (curr->i > 0)
 	{
--- libft/libft.h
+++ libft/libft.h
@@ -63,6 +63,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
+void 				*ft_memjoin(void const *s1, void const *s2, size_t n1, size_t n2);
 char				*ft_itoa(int n);
 char				*ft_strtrim(char const *s);
 int					ft_strequ(char const *s1, char const *s2);
